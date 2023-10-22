6704d87b296dfe3022c5538303c8f9b6d1dadd7b
Manav Kaushal <manavkaushal756@gmail.com>
2023-10-22 14:08:20 +0530

    authentication and synchronization complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import ClientProviders from "@/components/ClientProviders";
 import { appConfig } from "@/lib/appConfig";
+import FirebaseAuthProvider from "@/components/FirebaseAuthProvider";
 
 export const metadata: Metadata = {
   title: appConfig.name,
@@ -19,15 +20,17 @@ export default function RootLayout({
     <ClientProviders>
       <html lang="en">
         <body className="flex flex-col min-h-screen">
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Header />
-            {children}
-          </ThemeProvider>
+          <FirebaseAuthProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <Header />
+              {children}
+            </ThemeProvider>
+          </FirebaseAuthProvider>
         </body>
       </html>
     </ClientProviders>
---
+++ components/FirebaseAuthProvider.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { auth } from "@/firebase";
+import { signInWithCustomToken } from "firebase/auth";
+import { Session } from "next-auth";
+import { useSession } from "next-auth/react";
+import React, { useEffect } from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+async function syncFirebaseAuth(session: Session) {
+  if (session && session.firebaseToken) {
+    try {
+      await signInWithCustomToken(auth, session.firebaseToken);
+    } catch (error) {
+      console.error("Error signing in with custom token ->", error);
+    }
+  } else {
+    auth.signOut();
+  }
+}
+
+const FirebaseAuthProvider = ({ children }: Props) => {
+  const { data: session } = useSession();
+
+  // Sync next-auth session with firebase
+  useEffect(() => {
+    if (!session) return;
+
+    syncFirebaseAuth(session);
+  }, [session]);
+
+  return <>{children}</>;
+};
+
+export default FirebaseAuthProvider;
