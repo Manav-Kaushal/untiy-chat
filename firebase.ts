a18b2d09c6496570fce8c5e4a9aa1a581754d21b
Manav Kaushal <manavkaushal756@gmail.com>
2023-10-22 13:51:51 +0530

    firebase auth and firestore db integrated

---
+++ firebase.ts
@@ -0,0 +1,22 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFunctions } from "firebase/functions";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGwWKWrw2a7oFMjzXSRo3QhYJ8EmhLqCk",
+  authDomain: "unity-chat-app-247a3.firebaseapp.com",
+  projectId: "unity-chat-app-247a3",
+  storageBucket: "unity-chat-app-247a3.appspot.com",
+  messagingSenderId: "945709278736",
+  appId: "1:945709278736:web:b53f4887f2dcd0b3708c95",
+  measurementId: "G-H2S86L736Y",
+};
+
+// Initialize firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const functions = getFunctions(app);
+
+export { auth, db, functions };
